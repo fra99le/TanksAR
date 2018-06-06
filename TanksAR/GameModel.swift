9ff0bad2335737962de9c50a3318a9c7116fa78e
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2018-06-06 13:00:45 -0400

    Added file that will become the game model.

---
+++ TanksAR/GameModel.swift
@@ -0,0 +1,17 @@
+//
+//  GameModel.swift
+//  TanksAR
+//
+//  Created by Bryan Franklin on 6/6/18.
+//  Copyright © 2018 Doing Science To Stuff. All rights reserved.
+//
+
+import Foundation
+
+class GameModel {
+    // game board
+    
+    // tanks
+    
+    // score data
+}
