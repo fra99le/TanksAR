ba51a325b1b8fb2cbd571377350507a2a835691d
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2018-06-06 17:33:14 -0400

    Improved board placement UI.  Still not getting the correct center though.

--- TanksAR/GameViewController.swift
+++ TanksAR/GameViewController.swift
@@ -14,6 +14,9 @@ class GameViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var boardPlaced = false
+    @IBOutlet var tapToSelectLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,13 +35,22 @@ class GameViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        // Enable horizontal plane detection
+        configuration.planeDetection = [.horizontal]
+
+        // cause board placement to occur when view reappears
+        unplaceBoard()
+
         // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+
+        // cause board placement to occur when view reappears
+        unplaceBoard()
+
         // Pause the view's session
         sceneView.session.pause()
     }
@@ -73,5 +85,97 @@ class GameViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard !boardPlaced else { return }
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+
+        for node in node.childNodes {
+            node.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+            if let plane = node.geometry as? SCNPlane {
+                plane.width = CGFloat(planeAnchor.extent.x)
+                plane.height = CGFloat(planeAnchor.extent.z)
+            }
+        }
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard !boardPlaced else { return }
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        let floor = createFloor(planeAnchor)
+        node.addChildNode(floor)
+    }
+
+    func createFloor(_ planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let node = SCNNode()
+        
+        print("plane extents are \(planeAnchor.extent.x),\(planeAnchor.extent.z).")
+        let geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        node.geometry = geometry
+        
+        node.eulerAngles.x = -Float.pi / 2
+        node.opacity = 0.25
+        
+        return node
+    }
+
+    @IBOutlet var placeBoardGesture: UITapGestureRecognizer!
+    @IBAction func screenTapped(_ sender: UIGestureRecognizer) {
+        let touchLocation = sender.location(in: sceneView)
+        print("Screen tapped at \(touchLocation)")
+        let hitTestResult = sceneView.hitTest(touchLocation, types: [.existingPlaneUsingGeometry])
+        
+        for result in hitTestResult {
+            if !boardPlaced {
+                if let resultPlane =  result.anchor as? ARPlaneAnchor {
+                    placeBoard(resultPlane)
+                    break
+                }
+            } else {
+                print("Board already placed")
+            }
+        }
+    }
     
+    func clearAllPlanes() {
+        for child in sceneView.scene.rootNode.childNodes {
+            if (child.geometry as? SCNPlane) != nil {
+                child.removeFromParentNode()
+            }
+        }
+    }
+    
+    func placeBoard(_ atLocation: ARPlaneAnchor) {
+        // remove all candidate planes
+        clearAllPlanes()
+        
+        print("Placing board at \(atLocation)")
+        let node = SCNNode()
+        
+        //let boardPosition = atLocation.center
+        node.position = SCNVector3(atLocation.center)
+        
+        print("plane extents are \(atLocation.extent.x),\(atLocation.extent.z).")
+        let size = min(atLocation.extent.x,atLocation.extent.z)
+        let geometry = SCNPlane(width: CGFloat(size), height: CGFloat(size))
+        geometry.firstMaterial?.diffuse.contents = UIColor.green
+        node.geometry = geometry
+        
+        node.eulerAngles.x = -Float.pi / 2
+        node.opacity = 1.0
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        // update state
+        boardPlaced = true
+        placeBoardGesture.isEnabled = false
+        tapToSelectLabel.isHidden = true
+    }
+
+    func unplaceBoard() {
+        boardPlaced = false
+        placeBoardGesture.isEnabled = true
+        tapToSelectLabel.isHidden = false
+    }
 }
