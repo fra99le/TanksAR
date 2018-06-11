bd2db4a2c44498164cda50d4f5d7b28c2b7e1e7a
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2018-06-10 23:56:26 -0400

    Updated getPixel/setPixel to operate in terms of pixels instead of RGB values. Added guards for pixel locations.

--- TanksAR/ImageBuf.swift
+++ TanksAR/ImageBuf.swift
@@ -27,42 +27,32 @@ class ImageBuf {
         pixels = [Pixel](repeating: Pixel(r: 1, g: 1, b: 1, a: 1), count: width*height)
     }
     
-    func getPixel(x: Int, y: Int) -> (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+    func getPixel(x: Int, y: Int) -> Pixel {
         let offset = x + y*width
-        guard offset >= 0 && offset < pixels.count else { return (0,0,0,0) }
-        
-        let pixel = pixels[offset]
-        let r = pixel.r
-        let g = pixel.g
-        let b = pixel.b
-        let a = pixel.a
-        
-//        NSLog("getPixel(\(x),\(y)) -> \(r),\(g),\(b),\(a)")
-        return (r, g, b, a)
+        guard offset >= 0 && offset < pixels.count else { return Pixel(r: 0, g: 0, b: 0, a: 0) }
+        guard x < width && x >= 0 else { return Pixel(r: 0, g: 0, b: 0, a: 0) }
+
+        return pixels[offset]
     }
     
     func setPixel(x: Int, y: Int, r: Double, g: Double, b: Double, a: Double) {
-        setPixel(x: x, y: y, r: CGFloat(r), g: CGFloat(g), b: CGFloat(b), a: CGFloat(a))
+        setPixel(x: x, y: y, value: Pixel(r: CGFloat(r), g: CGFloat(g), b: CGFloat(b), a: CGFloat(a)))
     }
  
-    func setPixel(x: Int, y: Int, r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+    func setPixel(x: Int, y: Int, value: Pixel) {
         let offset = x + y*width
         guard offset >= 0 && offset < pixels.count else { return }
+        guard x < width && x >= 0 else { return }
 
-//        NSLog("setPixel(\(x),\(y)) <- \(r),\(g),\(b),\(a)")
-
-        let pixel = Pixel(r: r, g: g, b: b, a: a)
-        pixels[offset] = pixel
+        pixels[offset] = value
     }
     
     func copy(_ source: ImageBuf) {
-        //NSLog("image copy start")
         setSize(width: source.width, height: source.width)
     
         for i in 0..<source.pixels.count {
                 pixels[i] = source.pixels[i]
         }
-        //NSLog("image copy end")
     }
     
     func isPowerOfTwo(_ number: Int) -> Bool {
@@ -70,6 +60,7 @@ class ImageBuf {
     }
     
     // see: https://en.wikipedia.org/wiki/Diamond-square_algorithm
+    // also: https://stackoverflow.com/questions/7549883/smoothing-issue-with-diamond-square-algorithm
     func doDiamondSquare() {
         guard isPowerOfTwo(width-1) else { return }
         guard isPowerOfTwo(height-1) else { return }
@@ -81,81 +72,85 @@ class ImageBuf {
         setPixel(x: width-1, y: height-1, r: drand48(), g: 0, b: 0, a: 1.0)
 
         // make recursive call
-        doDiamondSquare(x1: 0, y1: 0, x2: width-1, y2: height-1)
+        doDiamondSquare(left: 0, right: width-1, top: 0, bottom: height-1)
     }
     
-    func doDiamondSquare(x1: Int, y1: Int, x2: Int, y2: Int) {
-        if (x2-1) <= x1 || (y2-1) <= y1 { return }
+    func doDiamondSquare(left: Int, right: Int, top: Int, bottom: Int) {
+        if (right-1) <= left || (bottom-1) <= top { return }
         
         // compute center location
-        let xc = (x1 + x2) / 2
-        let yc = (y1 + y2) / 2
-        let size = xc - x1
-//        if size > 200 {
-//            NSLog("center of \(x1),\(y1) and \(x2),\(y2) is \(xc),\(yc)")
-//        } else {
-//            NSLog("Returning for debugging purposes")
-//            return
-//        }
+        let centerX = (left + right) / 2
+        let centerY = (top + bottom) / 2
+        let size = centerX - left
         
         // compute center (diamond step)
-        diamondStep(x: xc, y: yc, size: size)
+        diamondStep(x: centerX, y: centerY, size: size)
         
         // compute edge centers (square step)
-        squareStep(x: xc, y: y1, size: size)
-        squareStep(x: x1, y: yc, size: size)
-        squareStep(x: xc, y: y2, size: size)
-        squareStep(x: x2, y: yc, size: size)
+        squareStep(x: centerX, y: top, size: size) // top
+        squareStep(x: left, y: centerY, size: size) // left
+        squareStep(x: centerX, y: bottom, size: size) // bottom
+        squareStep(x: right, y: centerY, size: size) // right
         
         // perform recursions
-        doDiamondSquare(x1: x1, y1: y1, x2: xc, y2: yc)
-        doDiamondSquare(x1: xc, y1: y1, x2: x2, y2: yc)
-        doDiamondSquare(x1: x1, y1: yc, x2: xc, y2: y2)
-        doDiamondSquare(x1: xc, y1: yc, x2: x2, y2: y2)
+        doDiamondSquare(left: left, right: centerX, top: top, bottom: centerY) // upper left
+        doDiamondSquare(left: centerX, right: right, top: top, bottom: centerY) // upper left
+        doDiamondSquare(left: left, right: centerX, top: centerY, bottom: bottom) // lower left
+        doDiamondSquare(left: centerX, right: right, top: centerY, bottom: bottom) // lower right
+    }
+    
+    func median(of: [Float]) -> Float {
+        let sorted = of.sorted()
+
+        let middle = sorted.count/2
+        if sorted.count % 2 == 0 {
+            return (sorted[middle]+sorted[middle+1])/2
+        } else {
+            return sorted[middle]
+        }
     }
     
     func diamondSquareStep(x: Int, y: Int, size: Int, pattern: [[Int]]) {
         var sum: CGFloat = 0.0
         var count = 0
-        let randomScale = CGFloat(size) / CGFloat(width)
+        //var values: [Float] = []
 
         for offsets in pattern {
-            let x = x + size * offsets[0]
-            let y = y + size * offsets[1]
+            let nx = x + size * offsets[0]
+            let ny = y + size * offsets[1]
             
-//            NSLog("trying to sample \(x),\(y)")
-            guard x >= 0 else { continue }
-            guard x < width else { continue }
-            guard y >= 0 else { continue }
-            guard y < height else { continue }
+            guard nx >= 0 else { continue }
+            guard nx < width else { continue }
+            guard ny >= 0 else { continue }
+            guard ny < height else { continue }
             
-            let (red: r, green: _, blue: _, alpha: _) = getPixel(x: x, y: y)
+            let r = getPixel(x: nx, y: ny).r
+            //values.append(Float(r))
             sum += r
             count += 1
         }
         
         let avg = sum / CGFloat(count)
+
+        let randomScale = CGFloat(size) / CGFloat(width)
         let noise = randomScale * CGFloat(drand48()) - (randomScale/2)
+        //let noise = CGFloat(0)
         let value = avg + noise
-//        NSLog("\(avg) = \(sum) / \(count), noise=\(noise)")
+        //let value = CGFloat(median(of: values)) + noise
         
-        setPixel(x: x, y: y, r: value, g: 0, b: 0, a: value)
+        setPixel(x: x, y: y, value: Pixel(r: value, g: 0, b: 0, a: value))
     }
     
     func diamondStep(x: Int, y: Int, size: Int) {
         diamondSquareStep(x: x, y: y, size: size,
-                    pattern: [[-1, -1], [-1,1], [1,-1], [1,1]]);
+                    pattern: [[-1, -1], [1,-1], [-1,1], [1,1]]);
     }
 
     func squareStep(x: Int, y: Int, size: Int) {
         diamondSquareStep(x: x, y: y, size: size,
-                    pattern: [[0, -1], [-1,0], [1,0], [0,1]]);
+                    pattern: [[0, -1], [-1, 0], [0, 1], [1,0]]);
     }
 
-    func normalize() {
-        
-    }
-    
     func fillUsingDiamondSquare(withMinimum: Float, andMaximum: Float) {
         guard isPowerOfTwo(width-1) else { return }
         guard isPowerOfTwo(height-1) else { return }
@@ -164,30 +159,50 @@ class ImageBuf {
         doDiamondSquare()
 
         // find min/max values
-        let (red: r, green: _, blue: _, alpha: _) = getPixel(x: 0, y: 0)
-        var minValue = r
-        var maxValue = r
-        for j in 0..<height {
-            for i in 0..<width {
-                let (red: r, green: _, blue: _, alpha: _) = getPixel(x: i, y: j)
-                minValue = (minValue>r) ? r : minValue
-                maxValue = (maxValue<r) ? r : maxValue
-            }
+        let pixel = getPixel(x: 0, y: 0)
+        var minValue = pixel.r
+        var maxValue = pixel.r
+        for i in 0..<pixels.count {
+            let r = pixels[i].r
+            minValue = (minValue>r) ? r : minValue
+            maxValue = (maxValue<r) ? r : maxValue
         }
 
-        // normalize to min/max values
+        // rescale to min/max values
         if maxValue-minValue >= 1 &&
             minValue<CGFloat(withMinimum) &&
             maxValue>CGFloat(andMaximum) {
-            for j in 0..<height {
-                for i in 0..<width {
-                    let (red: r, green: _, blue: _, alpha: _) = getPixel(x: i, y: j)
-                    
-                    //let nv = (andMaximum - withMinimum) * (r-minValue) / (maxValue-minValue) + withMinimum
-                    let nv = ( CGFloat(andMaximum - withMinimum) / CGFloat(maxValue-minValue) ) * CGFloat(r-minValue) + CGFloat(withMinimum)
-                    setPixel(x: i, y: j, r: nv, g: 0, b: 0, a: nv)
-                }
+            for i in 0..<pixels.count {
+                let r = pixels[i].r
+                
+                let nv = ( CGFloat(andMaximum - withMinimum) / CGFloat(maxValue-minValue) ) * CGFloat(r-minValue) + CGFloat(withMinimum)
+                pixels[i] = Pixel(r: nv, g: 0, b: 0, a: nv)
             }
         }
+    }
+    
+    func asUIImage() -> UIImage {
+        // see: http://blog.human-friendly.com/drawing-images-from-pixel-data-in-swift
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), true, 1);
+
+        NSLog("Converting \(width)x\(height) image to a UIImage.")
+        var image = UIImage()
+        if let context = UIGraphicsGetCurrentContext() {
+            for i in 0 ..< width {
+                for j in 0 ..< height {
+                    let pixel = getPixel(x: i, y: j)
+
+                    context.setFillColor(red: pixel.r, green: pixel.g, blue: pixel.b, alpha: pixel.a)
+
+                    context.fill(CGRect(x: CGFloat(i), y: CGFloat(j), width: 1, height: 1))
+                }
+            }
+            
+            image = UIGraphicsGetImageFromCurrentImageContext()!;
+        }
+
+        UIGraphicsEndImageContext();
+        
+        return image
     }
 }
