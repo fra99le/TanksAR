6de2fc6b45934d126b7886d542693a3e7c0228a9
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2018-06-24 03:03:45 -0400

    Added code to animate post explosion board updates.  Doesn't seem to work yet.

--- TanksAR/GameViewTrigDrawer.swift
+++ TanksAR/GameViewTrigDrawer.swift
@@ -12,6 +12,7 @@ import SceneKit
 class GameViewTrigDrawer : GameViewDrawer {
  
     var surface = SCNNode()
+    var dropSurface = SCNNode()
     var topNode = SCNNode()
     var bottomNode = SCNNode()
     var leftNode = SCNNode()
@@ -166,6 +167,287 @@ class GameViewTrigDrawer : GameViewDrawer {
         currTime = animateShell(fireResult: fireResult, at: currTime)
         currTime = animateExplosion(fireResult: fireResult, at: currTime)
         
+        let edgeSize = CGFloat(gameModel.board.boardSize / numPerSide)
+        
+        // draw dropping surfaces
+        var dropVertices0: [SCNVector3] = []
+        var dropVertices1: [SCNVector3] = []
+        var dropIndices: [CInt] = []
+        //var dropNormals: [SCNVector3] = []
+        var dropNeeded = false
+        for i in 0...numPerSide {
+            for j in 0...numPerSide {
+                let xArr = [CGFloat(i)*edgeSize, CGFloat(i)*edgeSize, CGFloat(i+1)*edgeSize, CGFloat(i+1)*edgeSize]
+                let yArr = [CGFloat(j)*edgeSize, CGFloat(j+1)*edgeSize, CGFloat(j+1)*edgeSize, CGFloat(j)*edgeSize]
+                var currentArr: [CGFloat] = []
+                var topArr: [CGFloat] = []
+                var middleArr: [CGFloat] = []
+                var bottomArr: [CGFloat] = []
+                var dropArr: [Bool] = []
+                var displaceArr: [Bool] = []
+                for k in 0...3 {
+                    let x = xArr[k]
+                    let y = yArr[k]
+                    
+                    // get elevations for each vertex
+                    let current = gameModel.getElevation(fromMap: fireResult.old, longitude: Int(x), latitude: Int(y))
+                    let top = gameModel.getElevation(fromMap: fireResult.top, longitude: Int(x), latitude: Int(y))
+                    let middle = gameModel.getElevation(fromMap: fireResult.middle, longitude: Int(x), latitude: Int(y))
+                    let bottom = gameModel.getElevation(fromMap: fireResult.bottom, longitude: Int(x), latitude: Int(y))
+                    //NSLog("k: \(k), current: \(current), top: \(top), middle: \(middle), bottom: \(bottom)")
+
+                    // record elevations for each point
+                    currentArr.append(CGFloat(current))
+                    topArr.append(CGFloat(top))
+                    middleArr.append(CGFloat(middle))
+                    bottomArr.append(CGFloat(bottom))
+
+                    // record booleans for different animation conditions
+                    dropArr.append(top > middle && middle > bottom)
+                    displaceArr.append(bottom != current)
+                }
+                if dropArr.count > 0 {
+                    dropNeeded = true
+                }
+                
+                // check for and add complete triangle for dropping portion
+                for p in [[0,1,2], [0,2,3]] {
+                    
+                    // check how many points are dropping
+                    var dropIdxs: [Int] = []
+                    var noDropIdxs: [Int] = []
+                    var displaceIdxs: [Int] = []
+                    var noDisplaceIdxs: [Int] = []
+                    for idx in p {
+                        if dropArr[idx] {
+                            dropIdxs.append(idx)
+                        } else {
+                            noDropIdxs.append(idx)
+                        }
+                        if displaceArr[idx] {
+                            displaceIdxs.append(idx)
+                        } else {
+                            noDisplaceIdxs.append(idx)
+                        }
+                    }
+                    
+                    let numDropping = dropIdxs.count
+                    let numDisplaced = displaceIdxs.count
+                    if numDropping == 3 {
+                        // entire triangle is dropping
+                        // add both top and middle
+                        for k in p {
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[k], yArr[k], topArr[k])))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[k], yArr[k],
+                                                                           bottomArr[k] + (topArr[k]-middleArr[k]))))
+                            dropIndices.append(CInt(dropVertices0.count-1))
+                        }
+                        for k in p {
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[k], yArr[k], middleArr[k])))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[k], yArr[k], bottomArr[k])))
+                            dropIndices.append(CInt(dropVertices0.count-1))
+                        }
+                    } else if numDropping == 2 {
+                        // check to see if dropping vertices should be connected
+                        let noDrop = noDropIdxs[0]
+                        if displaceArr[noDrop] {
+                            // 3rd vertex is unaffected, so attach other two via a triangle
+                            for k in dropIdxs {
+                                dropVertices0.append(fromModelSpace(SCNVector3(xArr[k], yArr[k], topArr[k])))
+                                dropVertices1.append(fromModelSpace(SCNVector3(xArr[k], yArr[k],
+                                                                               bottomArr[k] + (topArr[k]-middleArr[k]))))
+                                dropIndices.append(CInt(dropVertices0.count-1))
+                            }
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[noDrop], yArr[noDrop],
+                                                                           currentArr[noDrop])))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[noDrop], yArr[noDrop],
+                                                                           currentArr[noDrop])))
+                            dropIndices.append(CInt(dropVertices0.count-1))
+                            for k in dropIdxs {
+                                dropVertices0.append(fromModelSpace(SCNVector3(xArr[k], yArr[k], middleArr[k])))
+                                dropVertices1.append(fromModelSpace(SCNVector3(xArr[k], yArr[k], bottomArr[k])))
+                                dropIndices.append(CInt(dropVertices0.count-1))
+                            }
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[noDrop], yArr[noDrop],
+                                                                           currentArr[noDrop])))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[noDrop], yArr[noDrop],
+                                                                           currentArr[noDrop])))
+                            dropIndices.append(CInt(dropVertices0.count-1))
+                        } else {
+                            // 3rd vertex displaced, so leave unattached.
+                            // instead attach four moving vertices (top to middle) via triangles
+                            let index = CInt(dropVertices0.count)
+                            let idx1 = dropIdxs[0]
+                            let idx2 = dropIdxs[1]
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx1], yArr[idx1], topArr[idx1])))
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx1], yArr[idx1], middleArr[idx1])))
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx2], yArr[idx2], topArr[idx2])))
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx2], yArr[idx2], middleArr[idx2])))
+
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx1], yArr[idx1], bottomArr[idx1] + (topArr[idx1]-middleArr[idx1]))))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx1], yArr[idx1], bottomArr[idx1])))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx2], yArr[idx2], bottomArr[idx2] + (topArr[idx2]-middleArr[idx2]))))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx2], yArr[idx2], bottomArr[idx2])))
+                            
+                            dropIndices.append(index)
+                            dropIndices.append(index+2)
+                            dropIndices.append(index+1)
+
+                            dropIndices.append(index)
+                            dropIndices.append(index+2)
+                            dropIndices.append(index+3)
+                        }
+                    } else if numDropping == 1  && numDisplaced == 0 {
+                        // check to see if dropping vertex should be connected
+                        let idx1 = noDisplaceIdxs[0]
+                        let idx2 = noDisplaceIdxs[1]
+                        let idx3 = dropIdxs[0]
+                        if !displaceArr[idx1] && !displaceArr[idx2] {
+                            // two vertices are unaffected, so attach other one via a triangle
+                            
+                            // add top triangle
+                            var index = CInt(dropVertices0.count)
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx1], yArr[idx1], topArr[idx1])))
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx2], yArr[idx2], topArr[idx2])))
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx3], yArr[idx3], topArr[idx3])))
+
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx1], yArr[idx1], topArr[idx1])))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx2], yArr[idx2], topArr[idx2])))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx3], yArr[idx3],
+                                                                           bottomArr[idx3] + (topArr[idx3]-middleArr[idx3]))))
+
+                            dropIndices.append(index)
+                            dropIndices.append(index+2)
+                            dropIndices.append(index+1)
+
+                            // add bottom triangle
+                            index = CInt(dropVertices0.count)
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx1], yArr[idx1], topArr[idx1])))
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx2], yArr[idx2], topArr[idx2])))
+                            dropVertices0.append(fromModelSpace(SCNVector3(xArr[idx3], yArr[idx3], middleArr[idx3])))
+                            
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx1], yArr[idx1], topArr[idx1])))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx2], yArr[idx2], topArr[idx2])))
+                            dropVertices1.append(fromModelSpace(SCNVector3(xArr[idx3], yArr[idx3], bottomArr[idx3])))
+                            
+                            dropIndices.append(index)
+                            dropIndices.append(index+2)
+                            dropIndices.append(index+1)
+
+                        } else {
+                            // two vertices are displaced, so leave unattached.
+                            // this entire case is handled by the deformation of the final surface (i.e. bottom) layer.
+                        }
+
+                    } else if numDropping == 1  && numDisplaced == 1 {
+                        NSLog("i,j=\(i),\(j): numDropping=\(numDropping) and numDisplaced=\(numDisplaced), don't know how to handle this")
+                    }
+                }
+            }
+        }
+        
+        // draw new static bottom surface
+        var bottomVertices: [SCNVector3] = []
+        var bottomIndices: [CInt] = []
+        //var bottomNormals: [SCNVector3] = []
+        var pos: CInt = 0
+        for i in 0...numPerSide {
+            for j in 0...numPerSide {
+
+                // consruct new bottom surface
+                let x = CGFloat(i)*edgeSize
+                let y = CGFloat(j)*edgeSize
+                let z = gameModel.getElevation(fromMap: fireResult.bottom, longitude: Int(x), latitude: Int(y))
+                bottomVertices.append(fromModelSpace(SCNVector3(x,y,CGFloat(z))))
+                
+                if i < numPerSide && j < numPerSide {
+                    bottomIndices.append(pos)
+                    bottomIndices.append(pos+1)
+                    bottomIndices.append(pos+CInt(numPerSide)+2)
+                    
+                    bottomIndices.append(pos)
+                    bottomIndices.append(pos+CInt(numPerSide)+2)
+                    bottomIndices.append(pos+CInt(numPerSide)+1)
+                }
+                pos += 1
+            }
+        }
+        NSLog("\(dropVertices0.count) drop vertices, \(dropIndices.count) drop indices")
+        
+        if dropNeeded {
+            // create geometry for surface
+            let dropSource0 = SCNGeometrySource(vertices: dropVertices0)
+            let dropSource1 = SCNGeometrySource(vertices: dropVertices1)
+            let elements = SCNGeometryElement(indices: dropIndices, primitiveType: .triangles)
+            let dropGeometry0 = SCNGeometry(sources: [dropSource0], elements: [elements])
+            let dropGeometry1 = SCNGeometry(sources: [dropSource1], elements: [elements])
+            dropGeometry0.firstMaterial?.diffuse.contents = UIColor.red
+            dropGeometry1.firstMaterial?.diffuse.contents = UIColor.blue
+            
+            // add drop surface to scene
+            dropSurface.removeFromParentNode()
+            dropSurface = SCNNode(geometry: dropGeometry0)
+            dropSurface.opacity = 0
+            dropSurface.morpher = SCNMorpher()
+            dropSurface.morpher?.targets = [dropGeometry0, dropGeometry1]
+            board.addChildNode(dropSurface)
+        }
+        
+        NSLog("\(bottomVertices.count) bottom vertices, \(bottomIndices.count) bottom indices")
+
+        let bottomSource = SCNGeometrySource(vertices: bottomVertices)
+        let bottomElements = SCNGeometryElement(indices: bottomIndices, primitiveType: .triangles)
+        let bottomGeometry = SCNGeometry(sources: [bottomSource], elements: [bottomElements])
+        surface.morpher = SCNMorpher()
+        surface.morpher?.targets = [surface.geometry!, bottomGeometry]
+
+        
+        // animate the appearance and morphing of dropSurface
+        
+        // surface morphs to bottom surface
+        let animation1 = CABasicAnimation(keyPath: "morpher.weights[1]")
+        animation1.beginTime = currTime
+        animation1.fromValue = 0.0
+        animation1.toValue = 1.0
+        animation1.duration = 0.0
+        animation1.isRemovedOnCompletion = true
+        surface.addAnimation(animation1, forKey: "show bottom")
+
+        // drop animation
+        var dropAnimations: [CAAnimation] = []
+        
+        if dropNeeded {
+            // drop surface appears
+            let animation2 = CABasicAnimation(keyPath: "opacity")
+            animation2.beginTime = currTime
+            animation2.fromValue = 0.0
+            animation2.toValue = 1.0
+            animation2.duration = 0.0
+            dropAnimations.append(animation2)
+            
+            // drop surface morphs to second form
+            let animation3 = CABasicAnimation(keyPath: "morpher.weights[1]")
+            animation3.beginTime = currTime
+            animation3.fromValue = 0.0
+            animation3.toValue = 1.0
+            animation3.duration = dropTime
+            dropAnimations.append(animation3)
+            
+            let dropAnimation = CAAnimationGroup()
+            dropAnimation.animations = dropAnimations
+            dropAnimation.beginTime = 0
+            dropAnimation.duration = currTime + dropTime
+            dropAnimation.isRemovedOnCompletion = true
+            dropSurface.addAnimation(dropAnimation, forKey: "drop surface")
+        }
+        
+        NSLog("drop/short surface appear at time \(currTime)")
+        if dropNeeded {
+            currTime += dropTime
+        }
+        NSLog("board settled at time \(currTime).")
+        
+
         // wait for animations to end
         let delay = SCNAction.sequence([.wait(duration: currTime)])
         board.runAction(delay,
