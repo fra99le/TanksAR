4508d83f3ee1e6b74b8e7bef0f9af78b06b31955
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2018-07-03 04:53:36 -0400

    Added some code to help facilitate persistant state saving.

--- TanksAR/AppDelegate.swift
+++ TanksAR/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var saveController: UIViewController? = nil
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -27,6 +27,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        NSLog("\(#function)")
+        if let saveController = saveController as? MenuViewController {
+            saveController.saveStateFile()
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -39,6 +44,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
+        NSLog("\(#function)")
+        if let saveController = saveController as? MenuViewController {
+            saveController.saveStateFile()
+        }
     }
 
 
