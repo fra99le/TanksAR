ee843c51ccd885bbc2800a2208294f28c1c29f5d
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2019-05-27 19:59:48 -0400

    Added a license.

--- TanksAR/PlayerAI.swift
+++ TanksAR/PlayerAI.swift
@@ -5,6 +5,9 @@
 //  Created by Bryan Franklin on 6/12/18.
 //  Copyright © 2018 Doing Science To Stuff. All rights reserved.
 //
+//   This Source Code Form is subject to the terms of the Mozilla Public
+//   License, v. 2.0. If a copy of the MPL was not distributed with this
+//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Foundation
 import SceneKit
